505af1132365629a36858d8554a2b40591117c82
muneeb-babar <muneeb63ahmad63@gmail.com>
2024-06-11 22:19:33 +0500

    Cards animation

--- src/components/Singlecard.jsx
+++ src/components/Singlecard.jsx
@@ -1,23 +1,49 @@
-import '../css/Singlecard.css';
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../css/Singlecard.css';
 
 const Singlecard = ({ path, title, description, route }) => {
+  const [animated, setAnimated] = useState(false);
   const navigate = useNavigate();
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setAnimated(true);
+        }
+      },
+      { threshold: 0.5 } // Adjust threshold as needed
+    );
+
+    if (cardRef.current) {
+      observer.observe(cardRef.current);
+    }
+
+    return () => {
+      if (cardRef.current) {
+        observer.unobserve(cardRef.current);
+      }
+    };
+  }, []);
 
   const handleClick = () => {
     navigate(route);
   };
 
   return (
-    <div className="card" onClick={handleClick} style={{ cursor: 'pointer' }}>
+    <div
+      className={`card ${animated ? 'animate' : ''}`}
+      ref={cardRef}
+      onClick={handleClick}
+      style={{ cursor: 'pointer' }}
+    >
       <div className="header">
-        <img src={path} alt={title} className='card-img'/>
+        <img src={path} alt={title} className="card-img" />
       </div>
       <h4>{title}</h4>
-      <p className="message">
-        {description}
-      </p>
+      <p className="message">{description}</p>
     </div>
   );
 };
