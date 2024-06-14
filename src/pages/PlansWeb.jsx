57f64cce4e472ca8b957172b540b81de72d14fde
muneeb-babar <muneeb63ahmad63@gmail.com>
2024-06-14 19:24:05 +0500

    web services page

--- src/pages/PlansWeb.jsx
+++ src/pages/PlansWeb.jsx
@@ -1,53 +1,73 @@
-import * as React from 'react'
-import { useState } from 'react'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
-import { CardActionArea } from '@mui/material'
-import '../css/Plans.css'
-import ModalformBasicWeb from '../components/Modal/web-dev/Modal-Web-Basic'
-import ModalformStandardWeb from '../components/Modal/web-dev/Modal-Web-Standard'
-import ModalformPremiumWeb from '../components/Modal/web-dev/Modal-Web-Premium'
+import * as React from "react";
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { CardActionArea } from "@mui/material";
+import "../css/Plans.css";
+import "../css/planweb.css";
+import ModalformBasicWeb from "../components/Modal/web-dev/Modal-Web-Basic";
+import ModalformStandardWeb from "../components/Modal/web-dev/Modal-Web-Standard";
+import ModalformPremiumWeb from "../components/Modal/web-dev/Modal-Web-Premium";
 
 export default function PlansWeb({ plans }) {
-  const [showBasic, setShowBasic] = useState(false)
-  const [showStandard, setShowStandard] = useState(false)
-  const [showPremium, setShowPremium] = useState(false)
-  const [selectedPlan, setSelectedPlan] = useState('')
+  const [showBasic, setShowBasic] = useState(false);
+  const [showStandard, setShowStandard] = useState(false);
+  const [showPremium, setShowPremium] = useState(false);
+  const [selectedPlan, setSelectedPlan] = useState("");
 
-  const handleCloseBasic = () => setShowBasic(false)
-  const handleCloseStandard = () => setShowStandard(false)
-  const handleClosePremium = () => setShowPremium(false)
+  const handleCloseBasic = () => setShowBasic(false);
+  const handleCloseStandard = () => setShowStandard(false);
+  const handleClosePremium = () => setShowPremium(false);
 
   const handleShow = (planName) => {
-    setSelectedPlan(planName)
-    if (planName === ' Basic ') setShowBasic(true)
-    if (planName === 'Standard') setShowStandard(true)
-    if (planName === 'Premium') setShowPremium(true)
-  }
+    setSelectedPlan(planName);
+    if (planName === " Basic ") setShowBasic(true);
+    if (planName === "Standard") setShowStandard(true);
+    if (planName === "Premium") setShowPremium(true);
+  };
 
   return (
     <>
-      <div className='plans-container container'>
+      <div className="main-container container">
+        <h2>SoftMark Solutions - Building Your Digital Future</h2>
+        <h6 className="sub-paragraph">
+          At SoftMark Solutions, we recognize the critical role a well-crafted
+          website plays in your business success. Our expert team specializes in
+          creating responsive, user- friendly, and visually stunning websites
+          that not only captivate your audience but also drive business growth.
+          Whether you need a simple informational site or a complex e-commerce
+          platform, we are dedicated to bringing your vision to life and
+          ensuring your online presence stands out in a crowded digital
+          landscape
+        </h6>
+      </div>
+
+      <div className="card-heading-service">
+        <h2 className="heading-underline-service">Our Web Development Services</h2>
+      </div>
+
+    
+      <div className="plans-container container">
         {plans.map((plan, index) => (
           <Card className={`plan-card plan-card-${index}`} key={index}>
             <CardActionArea>
               <CardContent>
-                <div className='plans-cont'>
-                  <div className='plan-header'>
-                    <span className='plan-title'>{plan.name}</span>
+                <div className="plans-cont">
+                  <div className="plan-header">
+                    <span className="plan-title">{plan.name}</span>
                     {/* <span className='plan-price'>{`$${plan.price}/mo`}</span> */}
                     <button
-                      className='select-button'
+                      className="select-button"
                       onClick={() => handleShow(plan.name)}
                     >
                       START YOUR PLAN
                     </button>
                   </div>
-                  <div className='plan-features-container'>
-                    <ul className='plan-features'>
+                  <div className="plan-features-container">
+                    <ul className="plan-features">
                       {plan.features.map((feature, i) => (
                         <li key={i}>
-                          <i className='fa fa-check-circle'></i> {feature}
+                          <i className="fa fa-check-circle"></i> {feature}
                         </li>
                       ))}
                     </ul>
@@ -61,21 +81,21 @@ export default function PlansWeb({ plans }) {
 
       <ModalformBasicWeb
         isOpened={showBasic}
-        heading={selectedPlan + ' Plan'}
+        heading={selectedPlan + " Plan"}
         handleClose={handleCloseBasic}
       />
 
       <ModalformStandardWeb
         isOpened={showStandard}
-        heading={selectedPlan + ' Plan'}
+        heading={selectedPlan + " Plan"}
         handleClose={handleCloseStandard}
       />
 
       <ModalformPremiumWeb
         isOpened={showPremium}
-        heading={selectedPlan + ' Plan'}
+        heading={selectedPlan + " Plan"}
         handleClose={handleClosePremium}
       />
     </>
-  )
+  );
 }
