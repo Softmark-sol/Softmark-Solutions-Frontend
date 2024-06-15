0dbe466102d4acf0d99cf466d7e61f4bc1ba492d
Rehman810 <rehmankp196@gmail.com>
2024-06-15 06:44:16 -0700

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import HeroSection from '../components/HeroSection'
-import Cards from '../components/Cards'
-import OurClients from '../components/Slider'
-import ContactForm from '../components/ContactForm'
-import ScrollUpButton from '../components/ScrollUpButton'
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import Cards from "../components/Cards";
+import OurClients from "../components/Slider";
+import ContactForm from "../components/ContactForm";
+import ScrollUpButton from "../components/ScrollUpButton";
 
 const Home = () => {
   return (
     <>
-      <HeroSection/>
-      <Cards/>
-      <OurClients/>
-      <ContactForm/>
-      <ScrollUpButton/>
+      <HeroSection />
+      <Cards heading={`Our Services`}/>
+      <OurClients />
+      <ContactForm  />
+      <ScrollUpButton />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
