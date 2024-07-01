7ccdc7815674eaac9d194863bed423c4dc048f31
Rehman810 <rehmankp186@gmail.com>
2024-07-01 09:55:54 -0700

    change api

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 const API_CONFIG = {
-    apiKey: `https://02d8-2400-adc1-1c7-5400-f8fb-be41-2895-9eac.ngrok-free.app`,
+    apiKey: `https://a325-2400-adc1-1c7-5400-c5dd-f1ac-d10d-c967.ngrok-free.app`,
   };
   
   export default API_CONFIG;
