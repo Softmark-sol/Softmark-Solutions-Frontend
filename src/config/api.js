80791a62e2c7a2f8cd06d648fcf156e4efc06a16
Rehman810 <rehmankp186@gmail.com>
2024-07-10 10:36:03 -0700

    changes

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,6 @@
 const API_CONFIG = {
-    apiKey: `https://4fd8-2400-adc1-1c7-5400-65c7-23f9-8a78-1df2.ngrok-free.app`,
+    // apiKey: `https://4fd8-2400-adc1-1c7-5400-65c7-23f9-8a78-1df2.ngrok-free.app`,
+    apiKey: `https://d224-202-47-32-176.ngrok-free.app`,
   };
   
   export default API_CONFIG;
