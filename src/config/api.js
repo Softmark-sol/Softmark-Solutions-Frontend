76808265c1d1917a60cca922a8a7d799490fa42c
asghar-0017 <asghar.usman0017@gmail.com>
2024-07-15 00:06:38 +0500

    api

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 const API_CONFIG = {
     // apiKey: `https://4fd8-2400-adc1-1c7-5400-65c7-23f9-8a78-1df2.ngrok-free.app`,
-    apiKey: `https://18b9-202-47-32-176.ngrok-free.app`,
-    // apiKey: `52.71.145.45`,
+    // apiKey: `https://18b9-202-47-32-176.ngrok-free.app`,
+    apiKey: `52.71.145.45`,
   };
   
   export default API_CONFIG;
