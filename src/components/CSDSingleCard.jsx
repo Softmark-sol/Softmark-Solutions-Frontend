d3aa7dce54b0c41139f6e460ee62436b637bc5e0
Rehman810 <rehmankp186@gmail.com>
2024-07-26 08:48:30 -0700

    customSoftware hero and cards done.

---
+++ src/components/CSDSingleCard.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../css/CSD.css";
+
+const CSDSingleCard = ({icon, heading, text}) => {
+  return (
+    <div
+      className="container csd-single-card"
+    >
+      <div
+        style={{
+          backgroundColor: "#f1f3f299",
+          borderRadius: "50px",
+          padding: "15px",
+        }}
+      >
+        {icon}
+      </div>
+      <h5 style={{ fontWeight: "bold", textAlign: "left", paddingTop: "20px" }}>
+        {heading}
+      </h5>
+      <p style={{ textAlign: "left", paddingTop: "20px", fontSize: ".8rem" }}>
+        {text}
+      </p>
+    </div>
+  );
+};
+
+export default CSDSingleCard;
--- src/pages/customSoftware.jsx
+++ src/pages/customSoftware.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React from "react";
+import Typewriter from "../components/TypeWriter.jsx";
+import CSD from "../assets/images/csd.png";
+import "../css/CSD.css";
+import CSDCards from "../components/CSDCards.jsx";
+import { useNavigate } from "react-router-dom";
 
 const CustomSoftware = () => {
+    const navigate = useNavigate()
   return (
-    <div>
-     CustomSoftware 
-    </div>
-  )
-}
+    <>
+      <div className="main-container container">
+        <h2>
+          <Typewriter
+            strings={["Tailored Solutions for Your Business Needs"]}
+          />
+        </h2>
+        <div className="horizontal-fram-logo">
+          <div className="csd-hero">
+            <h5 className="main-paragrah-csd main-paragrah">
+              At SoftMark Solutions, we understand that every business is
+              unique. That’s why we offer custom software development services
+              tailored to meet your specific requirements. Whether you’re a
+              small business or a large enterprise, our team of expert
+              developers will work closely with you to create software solutions
+              that drive efficiency, innovation, and growth.
+              <br />
+              <div class="buttons" onClick={()=>navigate("/contactUs")}>
+                <button class="btn">
+                  <span></span>
+                  <p
+                    data-text="Get Proposal"
+                    data-title="Contact Now"
+                  ></p>
+                </button>
+              </div>
+            </h5>
+            <div className="csd-hero-img">
+              <img
+                src={CSD}
+                alt="csd"
+                style={{ width: "100%", height: "100%" }}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <CSDCards />
+    </>
+  );
+};
 
-export default CustomSoftware
+export default CustomSoftware;
