3fa9b32c00c9ce9e87ab0f8232614e758a83ac69
asghar-0017 <asghar.usman0017@gmail.com>
2024-09-19 00:06:51 +0500

    build

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Router from "./config/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { initFacebookPixel } from "./facebookPixel";
 // import MouseParticles from "react-mouse-particles";
 // import useIsMobile from "./hooks/useIsMobile";
-
 const App = () => {
   // const isMobile = useIsMobile();
-
+  useEffect(() => {
+    initFacebookPixel();
+  }, []);
   return (
     <>
       {/* {!isMobile && (
         <MouseParticles
           g={1.5}
           num={3}
-          color={["#4599b4", "#f3972b", "#4599b4", "#f3972b"]}
+          color={["#4599B4", "#F3972B", "#4599B4", "#F3972B"]}
           cull="col,image-wrapper"
         />
       )} */}
@@ -22,5 +24,4 @@ const App = () => {
     </>
   );
 };
-
 export default App;
